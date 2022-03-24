e85a6a7231823c8da99ce7538f5e0e48f7dd947c
Πrmd <pirmd@users.noreply.github.com>
2022-03-24 11:10:22 +0100

    Remove not-yet-ready epubcheck package

--- book/clean.go
+++
@@ -1,8 +0,0 @@
-package book
-
-// Clean makes sure that Book's metadata are in a reasonable format like pure text.
-//
-// dehtmlizer is pretty basic so only a sub-part of html formats are correctly
-// interpreted.
-func (b *Book) Clean() {
-}

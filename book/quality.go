acfd4e6b3ba5eff5173f9576eca93f4bd355223a
Πrmd <pirmd@users.noreply.github.com>
2022-03-26 17:19:07 +0100

    Add (preliminary) support in edit command to decide whether or not a manual edition is needed.

---
+++ book/quality.go
@@ -0,0 +1,42 @@
+package book
+
+// IsComplete gives an evaluation whether a Book's information seems complete
+// enough.
+// Objective is to provide a way for the tool to decide whether a human
+// intervention is a good idea.
+func (b *Book) IsComplete() bool {
+	if b.Title == "" {
+		Verbose.Printf("warn: Book's Title is empty")
+		return false
+	}
+
+	if len(b.Authors) == 0 {
+		Verbose.Printf("warn: Book has no Author(s)")
+		return false
+	}
+
+	if b.ISBN == "" {
+		Verbose.Printf("warn: Book has no ISBN")
+		return false
+	}
+
+	if b.Description == "" {
+		Verbose.Printf("warn: Book has no Description")
+	}
+
+	if b.Publisher == "" {
+		Verbose.Printf("warn: Book has no Publisher")
+	}
+
+	if b.PublishedDate == "" {
+		Verbose.Printf("warn: Book has no PublishedDate")
+	}
+
+	if (b.Series != "" && b.SeriesIndex == 0) ||
+		(b.SeriesIndex != 0 && b.Series == "") {
+		Verbose.Printf("warn: Book has incomplete Series information")
+		return false
+	}
+
+	return true
+}

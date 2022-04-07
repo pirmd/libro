e432f8463cc25e731530d1d7f0c7d404ac9fe589
Πrmd <pirmd@users.noreply.github.com>
2022-04-07 14:57:18 +0200

    Fix improper feedback to the user when setting new values

--- book/googlebooks.go
+++ book/googlebooks.go
@@ -67,87 +67,105 @@ func (b Book) toVolumeInfo() *googlebooks.VolumeInfo {
 
 // fromVolumeInfo populates Book's information from a googlebooks.VolumeInfo.
 func (b *Book) fromVolumeInfo(vi *googlebooks.VolumeInfo, override bool) {
-	if b.Title != "" && strings.EqualFold(b.Title, vi.Title) {
-		Debug.Printf("new Book's Title is different from the existing one (%s != %s)", vi.Title, b.Title)
-	}
-	if override || b.Title == "" {
-		Verbose.Printf("sets new Book's value: Title = %s", b.Title)
-		b.Title = vi.Title
-	}
-
-	if b.SubTitle != "" && strings.EqualFold(b.SubTitle, vi.SubTitle) {
-		Debug.Printf("new Book's SubTitle is different from the existing one (%s != %s)", vi.SubTitle, b.SubTitle)
-	}
-	if override || b.SubTitle == "" {
-		Verbose.Printf("sets new Book's value: SubTitle = %s", b.SubTitle)
-		b.SubTitle = vi.SubTitle
+	if vi.Title != "" {
+		if b.Title != "" && strings.EqualFold(b.Title, vi.Title) {
+			Debug.Printf("new Book's Title is different from the existing one (%s != %s)", vi.Title, b.Title)
+		}
+		if override || b.Title == "" {
+			Verbose.Printf("sets new Book's value: Title = %s", b.Title)
+			b.Title = vi.Title
+		}
 	}
 
-	if len(b.Authors) != 0 && strings.EqualFold(fmt.Sprint(b.Authors), fmt.Sprint(vi.Authors)) {
-		Debug.Printf("new Book's Authors is different from the existing one (%v != %v)", vi.Authors, b.Authors)
-	}
-	if override || len(b.Authors) == 0 {
-		Verbose.Printf("sets new Book's value: Authors = %v", vi.Authors)
-		b.Authors = append([]string{}, vi.Authors...)
-	}
-
-	if b.Publisher != "" && strings.EqualFold(b.Publisher, vi.Publisher) {
-		Debug.Printf("new Book's Publisher is different from the existing one (%s != %s)", vi.Publisher, b.Publisher)
-	}
-	if override || b.Publisher == "" {
-		Verbose.Printf("sets new Book's value: Publisher = %s", vi.Publisher)
-		b.Publisher = vi.Publisher
+	if vi.SubTitle != "" {
+		if b.SubTitle != "" && strings.EqualFold(b.SubTitle, vi.SubTitle) {
+			Debug.Printf("new Book's SubTitle is different from the existing one (%s != %s)", vi.SubTitle, b.SubTitle)
+		}
+		if override || b.SubTitle == "" {
+			Verbose.Printf("sets new Book's value: SubTitle = %s", b.SubTitle)
+			b.SubTitle = vi.SubTitle
+		}
 	}
 
-	if b.PublishedDate != "" && strings.EqualFold(b.PublishedDate, vi.PublishedDate) {
-		Debug.Printf("new Book's PublishedDate is different from the existing one (%s != %s)", vi.PublishedDate, b.PublishedDate)
-	}
-	if override || b.PublishedDate == "" {
-		Verbose.Printf("sets new Book's value: PublishedDate = %s", vi.PublishedDate)
-		b.PublishedDate = vi.PublishedDate
+	if len(vi.Authors) > 0 {
+		if len(b.Authors) != 0 && strings.EqualFold(fmt.Sprint(b.Authors), fmt.Sprint(vi.Authors)) {
+			Debug.Printf("new Book's Authors is different from the existing one (%v != %v)", vi.Authors, b.Authors)
+		}
+		if override || len(b.Authors) == 0 {
+			Verbose.Printf("sets new Book's value: Authors = %v", vi.Authors)
+			b.Authors = append([]string{}, vi.Authors...)
+		}
 	}
 
-	if b.Description != "" && strings.EqualFold(b.Description, vi.Description) {
-		Debug.Printf("new Book's Description is different from the existing one (%s != %s)", vi.Description, b.Description)
+	if vi.Publisher != "" {
+		if b.Publisher != "" && strings.EqualFold(b.Publisher, vi.Publisher) {
+			Debug.Printf("new Book's Publisher is different from the existing one (%s != %s)", vi.Publisher, b.Publisher)
+		}
+		if override || b.Publisher == "" {
+			Verbose.Printf("sets new Book's value: Publisher = %s", vi.Publisher)
+			b.Publisher = vi.Publisher
+		}
 	}
-	if override || b.Description == "" {
-		Verbose.Printf("sets new Book's value: Description = %s", vi.Description)
-		b.Description = vi.Description
+
+	if vi.PublishedDate != "" {
+		if b.PublishedDate != "" && strings.EqualFold(b.PublishedDate, vi.PublishedDate) {
+			Debug.Printf("new Book's PublishedDate is different from the existing one (%s != %s)", vi.PublishedDate, b.PublishedDate)
+		}
+		if override || b.PublishedDate == "" {
+			Verbose.Printf("sets new Book's value: PublishedDate = %s", vi.PublishedDate)
+			b.PublishedDate = vi.PublishedDate
+		}
+	}
+
+	if vi.Description != "" {
+		if b.Description != "" && strings.EqualFold(b.Description, vi.Description) {
+			Debug.Printf("new Book's Description is different from the existing one (%s != %s)", vi.Description, b.Description)
+		}
+		if override || b.Description == "" {
+			Verbose.Printf("sets new Book's value: Description = %s", vi.Description)
+			b.Description = vi.Description
+		}
 	}
 
 	isbn := getVolumeInfoISBN(vi)
-	if b.ISBN != "" && strings.EqualFold(b.ISBN, isbn) {
-		Debug.Printf("new Book's ISBN is different from the existing one (%s != %s)", isbn, b.ISBN)
-	}
-	if override || b.ISBN == "" {
-		Verbose.Printf("sets new Book's value: ISBN = %s", isbn)
-		b.ISBN = isbn
-	}
-
-	if b.Language != "" && strings.EqualFold(b.Language, vi.Language) {
-		Debug.Printf("new Book's Language is different from the existing one (%s != %s)", vi.Language, b.Language)
-	}
-	if override || b.Language == "" {
-		Verbose.Printf("sets new Book's value: Language = %s", vi.Language)
-		b.Language = vi.Language
+	if isbn != "" {
+		if b.ISBN != "" && strings.EqualFold(b.ISBN, isbn) {
+			Debug.Printf("new Book's ISBN is different from the existing one (%s != %s)", isbn, b.ISBN)
+		}
+		if override || b.ISBN == "" {
+			Verbose.Printf("sets new Book's value: ISBN = %s", isbn)
+			b.ISBN = isbn
+		}
 	}
 
-	if b.PageCount != 0 && b.PageCount != vi.PageCount {
-		Debug.Printf("new Book's PageCount is different from the existing one (%d != %d)", vi.PageCount, b.PageCount)
+	if vi.Language != "" {
+		if b.Language != "" && strings.EqualFold(b.Language, vi.Language) {
+			Debug.Printf("new Book's Language is different from the existing one (%s != %s)", vi.Language, b.Language)
+		}
+		if override || b.Language == "" {
+			Verbose.Printf("sets new Book's value: Language = %s", vi.Language)
+			b.Language = vi.Language
+		}
 	}
 
-	if override || b.PageCount == 0 {
-		Verbose.Printf("sets new value: PageCount = %d.", vi.PageCount)
-		b.PageCount = vi.PageCount
+	if vi.PageCount != 0 {
+		if b.PageCount != 0 && b.PageCount != vi.PageCount {
+			Debug.Printf("new Book's PageCount is different from the existing one (%d != %d)", vi.PageCount, b.PageCount)
+		}
+		if override || b.PageCount == 0 {
+			Verbose.Printf("sets new value: PageCount = %d", vi.PageCount)
+			b.PageCount = vi.PageCount
+		}
 	}
 
-	if len(b.Categories) != 0 && strings.EqualFold(fmt.Sprint(b.Categories), fmt.Sprint(vi.Subject)) {
-		Debug.Printf("new Book's Categories is different from the existing one (%v != %v)", vi.Subject, b.Categories)
-	}
-
-	if override || len(b.Categories) == 0 {
-		Verbose.Printf("sets new Book's value: Categories = %v", vi.Subject)
-		b.Categories = append([]string{}, vi.Subject...)
+	if len(vi.Subject) > 0 {
+		if len(b.Categories) != 0 && strings.EqualFold(fmt.Sprint(b.Categories), fmt.Sprint(vi.Subject)) {
+			Debug.Printf("new Book's Categories is different from the existing one (%v != %v)", vi.Subject, b.Categories)
+		}
+		if override || len(b.Categories) == 0 {
+			Verbose.Printf("sets new Book's value: Categories = %v", vi.Subject)
+			b.Categories = append([]string{}, vi.Subject...)
+		}
 	}
 }
 

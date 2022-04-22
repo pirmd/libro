ec738586c5d984b509589b4d3ea85379c92384c1
Πrmd <pirmd@users.noreply.github.com>
2022-04-22 18:01:41 +0200

    Add access to several variant of ISBN identifiers.

--- book/isbn.go
+++ book/isbn.go
@@ -7,6 +7,22 @@ import (
 	"unicode"
 )
 
+// ISBN13 returns the ISBN_13 identifier of Book.
+func (b Book) ISBN13() (string, error) {
+	if len(b.ISBN) == 13 {
+		return b.ISBN, nil
+	}
+	return toISBN13(b.ISBN)
+}
+
+// ISBN10 returns the ISBN_10 identifier of Book.
+func (b Book) ISBN10() (string, error) {
+	if len(b.ISBN) == 10 {
+		return b.ISBN, nil
+	}
+	return toISBN10(b.ISBN)
+}
+
 // NormalizeISBN returns a cleaned ISBN_13 identifier.
 // If isbn is in ISBN_10 format it will be converted to ISBN_13.
 // If isbn is in a non-supported format, the 'cleaned' ISBN is returned and an
@@ -83,6 +99,54 @@ func calcISBN13checkdigit(isbn13 string) (string, error) {
 
 	if len(isbn13) == 13 && isbn13[12:] != checkdigit {
 		return "", fmt.Errorf("mismatched check-digit between actual ISBN_13 (%s) and calculated one (%s)", isbn13[12:], checkdigit)
+	}
+	return checkdigit, nil
+}
+
+// toISBN10 tries to convert an ISBN_13 to an ISBN_10.
+// isbn13 is supposed to be a "cleaned" ISBN (only digits, no '-' or things
+// like that)
+// if isbn10 is provided with a 10th digit, check-digit calculation outcome
+// will be compared to this 10th digit and an error will be raised if they are
+// different.
+func toISBN10(isbn13 string) (string, error) {
+	if len(isbn13) != 13 || !strings.HasPrefix(isbn13, "978") {
+		return "", fmt.Errorf("convert to ISBN_10 failed: %s is not a suitable ISBN_13", isbn13)
+	}
+
+	isbn10 := isbn13[3:12]
+	checkdigit, err := calcISBN10checkdigit(isbn10)
+	if err != nil {
+		return "", err
+	}
+	return isbn10 + checkdigit, nil
+}
+
+// calcISBN10checkdigit calculates last check-digit of an ISBN_10.
+// isbn10 should be a 'cleaned' isbn10 without its last checksum digit.
+// if isbn13 is provided with a 13th digit, check-digit calculation outcome
+// will be compared to this 13th digit and an error will be raised if they are
+// different.
+func calcISBN10checkdigit(isbn10 string) (string, error) {
+	if len(isbn10) != 9 && len(isbn10) != 10 {
+		return "", fmt.Errorf("ISBN_10 check-digit calculation failed: %s is not a suitable ISBN_10", isbn10)
+	}
+
+	// from: https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-10_check_digit_calculation
+	var sum int
+	for i, c := range isbn10[:9] {
+		sum += int(c-'0') * (10 - i)
+	}
+
+	digit := (11 - sum%11) % 11
+
+	if digit == 10 {
+		return "X", nil
+	}
+	checkdigit := strconv.Itoa(digit)
+
+	if len(isbn10) == 10 && isbn10[9:] != checkdigit {
+		return "", fmt.Errorf("mismatched check-digit between actual ISBN_10 (%s) and calculated one (%s)", isbn10[9:], checkdigit)
 	}
 	return checkdigit, nil
 }
--- book/isbn_test.go
+++ book/isbn_test.go
@@ -60,3 +60,49 @@ func TestToISBN13(t *testing.T) {
 		}
 	}
 }
+
+func TestCalcISBN10checkdigit(t *testing.T) {
+	testCases := []string{
+		"0070350485", "007035958X", "0070393605", "0070418780",
+		"0072961945", "007296216X", "0072962216", "0072963050",
+		"0323025935", "0324007515", "0324066856", "0805366245",
+		"0805368515", "080537146X", "1418843709", "1558606041",
+		"155860832X",
+	}
+
+	for _, tc := range testCases {
+		want := tc[9:]
+		got, err := calcISBN10checkdigit(tc[:9])
+		if err != nil {
+			t.Errorf("Fail to calculate check-digit for %s: %v", tc, err)
+			continue
+		}
+
+		if want != got {
+			t.Errorf("fail to calculate check-digit for %s. Got: %v. Want: %v", tc, got, want)
+		}
+	}
+}
+
+func TestToISBN10(t *testing.T) {
+	testCases := []struct {
+		in  string
+		out string
+	}{
+		{in: "9782211238434", out: "2211238432"},
+		{in: "9780486285405", out: "0486285405"},
+		{in: "9782017064657", out: "2017064653"},
+	}
+
+	for _, tc := range testCases {
+		got, err := toISBN10(tc.in)
+		if err != nil {
+			t.Errorf("Fail to convert %s to ISBN_10: %v", tc.in, err)
+			continue
+		}
+
+		if tc.out != got {
+			t.Errorf("fail to convert %s to ISBN_10. Got: %v. Want: %v", tc.in, got, tc.out)
+		}
+	}
+}
